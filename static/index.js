ac035e814be9ba007284b0e848f9f1c74f455c4f
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-05-27 02:31:02 +0800

    Update load env format

--- static/index.js
+++ static/index.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: $os.getenv("FIREBASE_API_KEY"),
+    authDomain: $os.getenv("FIREBASE_AUTH_DOMAIN"),
+    databaseURL: $os.getenv("FIREBASE_DATABASE_URL"),
+    projectId: $os.getenv("FIREBASE_PROJECT_ID"),
+    storageBucket: $os.getenv("FIREBASE_STORAGE_BUCKET"),
+    messagingSenderId: $os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    appId: $os.getenv("FIREBASE_APP_ID"),
+    measurementId: $os.getenv("FIREBASE_MEASUREMENT_ID")
 };
 
 firebase.initializeApp(firebaseConfig);
